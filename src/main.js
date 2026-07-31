c6c9a2f72278a3952f34b897f11a1417bfd52065
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-07-31 04:55:07 +0300

    Final Project Responsive layout

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 import "./css/styles.css";
 import "./css/responsive.css";
-
 import Navigation from "./js/ui/Navigation.js";
 import Hero from "./js/ui/Hero.js";
 import FeatureCards from "./js/ui/FeatureCards.js";
 import Footer from "./js/ui/Footer.js";
 
 document.querySelector("#app").innerHTML = `
-
 ${Navigation()}
-
 ${Hero()}
-
 ${FeatureCards()}
-
 ${Footer()}
 
 `;
