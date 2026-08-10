ff1ff2c05111a716f7a42ce5cecb220c5b76cc7b
emmanuelbassey685 <emmanuelbassey685@yahoo.com>
2026-08-10 19:11:59 +0300

    Final Project On Healthcare Appointment Booking System

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import "./css/components.css";
 import "./css/forms.css";
 import "./css/dashboard.css";
 import "./css/responsive.css";
+
 import Layout from "./js/components/Layout.js";
 import Hero from "./js/components/Hero.js";
 import FeatureCards from "./js/components/FeatureCards.js";
+import { initNavigation } from "./js/modules/NavigationManager.js";
 
 const app = document.querySelector("#app");
 
@@ -13,3 +15,6 @@ app.innerHTML = Layout(`
     ${Hero()}
     ${FeatureCards()}
 `);
+
+// Initialize navigation after Layout has been inserted
+initNavigation();
